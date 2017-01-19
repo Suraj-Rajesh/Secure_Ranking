41c16e11628c6bad7b64afd2eb67d11a850b4fb6
Suraj-Rajesh <surajx.rajesh@gmail.com>
2017-01-18 20:20:03 -0800

    Plain & Encrypted Search Servers implemented

--- src/encrypted_search_server.py
+++ src/encrypted_search_server.py
@@ -1,10 +1,13 @@
 from generate_index import load_index
 from do_parser import stem_text
 import operator
+from paillier_crypto import load_keys
+from paillier.paillier import e_add, decrypt
 
 if __name__ == "__main__":
     # Load search index
-    index = load_index("../index/plain_index.pkl")
+    index = load_index("../index/encrypted_index.pkl")
+    (private_key, public_key) = load_keys("../keys/private_key.pkl", "../keys/public_key.pkl")
 
     # Start the server
     try:
@@ -19,10 +22,13 @@ if __name__ == "__main__":
                     keyword_search_index = index[keyword]
                     for filename, value in keyword_search_index.iteritems():
                         if filename in sort_index:
-                            sort_index[filename] = sort_index[filename] + value
+                            sort_index[filename] = e_add(public_key, sort_index[filename], value)
                         else:
                             sort_index[filename] = value
 
+            # Decrypt the sort index
+            for filename, value in sort_index.iteritems():
+                sort_index[filename] = decrypt(private_key, public_key, value)
             # Sort the final sort_index
             ranked_result = sorted(sort_index.items(), key=operator.itemgetter(1), reverse=True)
             # Print for now
--- src/generate_index.py
+++ src/generate_index.py
@@ -3,18 +3,19 @@ from collections import defaultdict
 from textblob import TextBlob as tb
 from tf_idf_generator import tf, n_containing, idf, tfidf
 import cPickle as pickle
+from paillier.paillier import *
+from paillier_crypto import load_keys
+import random
 
-corpus_textblobs = dict()
-plain_index = defaultdict(dict)
-
-def load_documents(directory):
+def load_documents(directory, corpus_textblobs):
     files = listdir(directory)
     for f in files:
         text = doc_to_text(directory + "/" + f)
         textblob = tb(text)
         corpus_textblobs[f] = textblob
 
-def index_generator(corpus_textblobs):
+def plain_index_generator(corpus_textblobs):
+    plain_index = defaultdict(dict)
     # For each file to be indexed
     for filename, textblob in corpus_textblobs.iteritems():
         # Calculate score of all words in the textblob corresponding to that file
@@ -22,12 +23,21 @@ def index_generator(corpus_textblobs):
         # Index each word into the plain search index
         for word in word_score_index:
             plain_index[word][filename] = word_score_index[word]
+    save_index("../index/plain_index.pkl", plain_index)
+
+def mask_encrypt(value, offset, public_key):
+    offseted = int(value * 100000) + offset
+    encrypted_value = encrypt(public_key, offseted)
+    return encrypted_value
 
 def encrypted_index_generator(plain_index_file):
+    (priv_key, pub_key) = load_keys("../keys/private_key.pkl", "../keys/public_key.pkl")
     plain_index = load_index(plain_index_file)
+    offset = random.randint(50, 100)
     for keyword, keyword_index in plain_index.iteritems():
         for filename, tf_idf in keyword_index.iteritems():
-            plain_index[keyword][filename] = encrypt(tf_idf)
+            plain_index[keyword][filename] = mask_encrypt(tf_idf, offset, pub_key)
+    save_index("../index/encrypted_index.pkl", plain_index)
 
 def save_index(filename, index):
     with open(filename, "wb") as output:
@@ -38,10 +48,10 @@ def load_index(index_file):
         index = pickle.load(inpt)
     return index
 
-if __name__ == "__main__":
-#    prepare_documents("../corpus/raw_documents", "../corpus/prepared_documents")
-#    load_documents("../corpus/prepared_documents")   
-#    index_generator(corpus_textblobs)
-#    save_index("plain_index.pkl", plain_index)
-    pl_index = load_index("plain_index.pkl")
-    print pl_index["propos"]
+def generate_plain_index_driver():
+    corpus_textblobs = dict()
+    load_documents("../corpus/prepared_documents", corpus_textblobs)
+    plain_index_generator(corpus_textblobs)
+
+def generate_encrypted_index_driver():
+    encrypted_index_generator("../index/plain_index.pkl")
